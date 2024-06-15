7384bf42f6faf615d9c4cb562fe7369b98ccbe72
SakshamKumar28 <148777071+SakshamKumar28@users.noreply.github.com>
2024-06-15 21:08:40 +0530

    Add files via upload

---
+++ Secret_code_generator.py
@@ -0,0 +1,33 @@
+import random as r
+import string as s
+
+
+def secret_code_converter(string):
+
+    if len(string) <= 3:
+        string = string[::-1]
+        return string
+    else:
+        s1 = string[1::]
+        #creating 3 random characters
+        c1 = r.choice(s.ascii_letters)
+        c2 = r.choice(s.ascii_letters)
+        c3 = r.choice(s.ascii_letters)
+        #creating list of these random characters
+        list_of_random_characters = [c1, c2, c3]
+        #shuffling list every time when new string is entered
+        r.shuffle(list_of_random_characters)
+        #creating list of new string (s1) and the first letter of main string
+        list = [s1, string[0]]
+        #creating a new list by adding created lists
+        final_list = list_of_random_characters + list
+        return "".join(final_list)
+
+
+
+#main program
+#taking input
+a = input("Enter String: ")
+#using function
+res = secret_code_converter(a)
+print(res)
